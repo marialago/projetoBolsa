e15b261e3c06676508c4f2ded27720d4aced9aa2
marialago <maria.lago@radixeng.com.br>
2022-06-15 13:33:11 -0300

    simplifica

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,23 +1,17 @@
 export class Negociacao {
 
     constructor(
-        private _data: Date, 
-        private _quantidade: number, 
-        private _valor: number) {}
+        public _data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number) {}
+
 
     get data(): Date {
-        return this._data;
+        const data = new Date(this._data.getTime());
+        return data;
     }
-
-    get quantidade(): number {
-        return this._quantidade;
-    }
-
-    get valor(): number {
-        return this._valor;
-    }
-
+    
     get volume(): number {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
