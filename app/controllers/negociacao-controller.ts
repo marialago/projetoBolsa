9e87adc99e23a5ab85ed0a993e7c74ff605120bb
marialago <maria.lago@radixeng.com.br>
2022-06-21 14:39:56 -0300

    ajustes

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -20,7 +20,11 @@ export class NegociacaoController {
     }
 
     public adiciona(): void{
-        const negociacao = this.criaNegociacao();
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         if(!this.diaUtil(negociacao.data)){
             this.mensagemView.update('apenas negociações em dias úteis são aceitas');
             return ;
@@ -33,14 +37,6 @@ export class NegociacaoController {
     private diaUtil (data: Date){
         return data.getDay() > DiaDaSemana.DOMINGO 
         && data.getDay() < DiaDaSemana.SABADO;
-    }
-
-    private criaNegociacao(): Negociacao{
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
     }
 
     private limparFormulario(): void {
